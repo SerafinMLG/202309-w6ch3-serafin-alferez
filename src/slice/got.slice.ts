52b17e4a9ba8c01323fa83579659e4e3c1fb7a4a
Serafin Alferez <malaga.carbon3@gmail.com>
2023-11-15 17:16:42 +0100

    Add thunk and slice

--- src/slice/got.slice.ts
+++ src/slice/got.slice.ts
@@ -1,34 +1,45 @@
 import { Character } from '../model/characters';
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { loadCharacterThunk, updateCharacterThunk } from './got.thunks';
 
 type CharacterState = {
   characters: Character[];
+  characterState: 'idle' | 'loading' | 'error';
 };
 
 const initialState: CharacterState = {
   characters: [],
+  characterState: 'idle',
 };
 
 const characterSlice = createSlice({
   name: 'character',
   initialState,
-  reducers: {
-    loadCharacters: (
-      state: CharacterState,
-      { payload }: PayloadAction<Character[]>
-    ) => {
-      state.characters = payload;
+  reducers: {},
+  
+  extraReducers: (builder) => {
+    builder.addCase(loadCharacterThunk.pending, (state: CharacterState) => {
+      state.characterState = 'loading';
       return state;
-    },
+    }),
+    
+    builder.addCase(loadCharacterThunk.fulfilled,
+      (state: CharacterState, { payload }: PayloadAction<Character[]>) => {
+        state.characters = payload;
+        state.characterState = 'idle';
+        return state;
+      }
+    );
+    
+    builder.addCase(
+      updateCharacterThunk.fulfilled,
+      (state: CharacterState, { payload }: PayloadAction<Character>) => {
+        state.characters[state.characters.findIndex((item) => item.id === payload.id)] =
+          payload;
+        return state;
+      } 
+    )
+    }
+  });
 
-    update: (state: CharacterState, { payload }: PayloadAction<Character>) => {
-      state.characters[
-        state.characters.findIndex((item) => item.id === payload.id)
-      ] = payload;
-      return state;
-    },
-  },
-});
-
-export default characterSlice.reducer;
-export const { loadCharacters, update } = characterSlice.actions;
+  export default characterSlice.reducer;
---
+++ src/slice/got.thunks.ts
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { ApiRepo } from '../services/api.repo';
+import { Character } from '../model/characters';
+
+
+type Params = {
+  repo: ApiRepo;
+  newCharacter: Partial<Character>;
+};
+
+export const loadCharacterThunk = createAsyncThunk<Character[],ApiRepo>(
+  'characters/load', 
+  async (repo) => {
+    const characters = await repo.getCharacters();
+    return characters
+  }
+);
+
+export const updateCharacterThunk = createAsyncThunk<Character,{repo: ApiRepo;
+id: Character['id'];
+updatedCharacter: Partial<Character>;
+}
+>('character/update', async ({repo, id, updatedCharacter})=> {
+  const finalCharacter = await repo.setCharacters(id, updatedCharacter);
+  return finalCharacter;
+});
